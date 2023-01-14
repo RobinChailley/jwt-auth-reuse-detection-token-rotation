44a4bfe09d3644bcc2be5f0ecbdb27389ec5d399
Robin Chailley <robin.chailley@origame.fr>
2023-01-14 20:27:48 +0100

    feat: inject dependencies on tests

--- src/auth/tests/auth.service.spec.ts
+++ src/auth/tests/auth.service.spec.ts
@@ -1,18 +1,30 @@
 import { AuthService } from '@auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '@users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService: UsersService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useFactory: () => ({}) },
+        { provide: JwtService, useFactory: () => ({}) },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+    expect(jwtService).toBeDefined();
   });
 });
--- src/users/tests/users.service.spec.ts
+++ src/users/tests/users.service.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserEntity } from '@users/entities/user.entity';
 import { UsersService } from '@users/users.service';
+import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repository: Repository<UserEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useFactory: () => ({}),
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get(getRepositoryToken(UserEntity));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(repository).toBeDefined();
   });
 });
