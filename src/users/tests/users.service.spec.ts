17dc93dc04bb58505786c7e6dd1a42ee40405cbb
Robin Chailley <robin.chailley@origame.fr>
2023-01-15 17:12:57 +0100

    tests: users.service

--- src/users/tests/users.service.spec.ts
+++ src/users/tests/users.service.spec.ts
@@ -27,4 +27,64 @@ describe('UsersService', () => {
     expect(service).toBeDefined();
     expect(repository).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should create and assign user properties', () => {
+      expect.assertions(4);
+      const user = service.create({
+        email: 'email',
+        hashPassword: 'hash',
+        salt: 'salt',
+      });
+
+      expect(user).toBeInstanceOf(UserEntity);
+      expect(user.email).toBe('email');
+      expect(user.hashPassword).toBe('hash');
+      expect(user.salt).toBe('salt');
+    });
+  });
+
+  describe('save', () => {
+    let user: UserEntity;
+    beforeEach(() => {
+      user = new UserEntity();
+      user.save = jest.fn();
+    });
+
+    it('should call entity.save()', async () => {
+      await service.save(user);
+
+      expect(user.save).toBeCalled();
+    });
+
+    it('shoud throw if entity.save() fails', async () => {
+      user.save = jest.fn().mockRejectedValue(new Error());
+
+      await expect(service.save(user)).rejects.toThrowError();
+    });
+  });
+
+  describe('findOneByEmail', () => {
+    beforeEach(() => {
+      repository.findOne = jest.fn();
+    });
+
+    it('should call repository.findOne', async () => {
+      await service.findOneByEmail('test');
+
+      expect(repository.findOne).toBeCalledWith({ where: { email: 'test' } });
+    });
+  });
+
+  describe('findOneById', () => {
+    beforeEach(() => {
+      repository.findOne = jest.fn();
+    });
+
+    it('should call repository.findOne', async () => {
+      await service.findOneById(30);
+
+      expect(repository.findOne).toBeCalledWith({ where: { id: 30 } });
+    });
+  });
 });
